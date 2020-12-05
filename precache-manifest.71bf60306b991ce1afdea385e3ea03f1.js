b9ec83c73f650c10fbbe2e455aeccb2d3aec08dc
Fredy Saul Teheran Tovar <ftehera@bancodebogota.com.co>
2020-12-04 21:37:03 -0500

    Updates

---
+++ precache-manifest.71bf60306b991ce1afdea385e3ea03f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4307045c140725e1e98a82038c268c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb2f103b2d4e924dd8f8",
+    "url": "/static/js/2.2ee4ef05.chunk.js"
+  },
+  {
+    "revision": "b1526fc4127dcbf18b8c2d0c0c3796f0",
+    "url": "/static/js/2.2ee4ef05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4d74f7a0813042fc14",
+    "url": "/static/js/main.dfbb8172.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "63b67a0a7a5d8de3679f3b67fafc54ec",
+    "url": "/static/media/logo.63b67a0a.svg"
+  },
+  {
+    "revision": "63b67a0a7a5d8de3679f3b67fafc54ec",
+    "url": "/static/media/logo_white.63b67a0a.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/info/precache-manifest.67fd02bda17ba16dc1cdd6825fca2f83.js"
+  "/precache-manifest.71bf60306b991ce1afdea385e3ea03f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/info/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
